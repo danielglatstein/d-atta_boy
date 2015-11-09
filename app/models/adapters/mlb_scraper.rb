9f801af881e265c617a4cb5a2e4174fda8bb3abf
danielglatstein <danielglatstein@gmail.com>
2015-11-09 09:26:57 -0500

    add teams to seed file

--- app/models/adapters/mlb_scraper.rb
+++ app/models/adapters/mlb_scraper.rb
@@ -1,7 +1,7 @@
 module Adapters
   class MlbScraper
 
-     def click_link(page, info)
+     def click_link_on(page, info)
         page.links.each do |link|
           if link.text.include?(info)
             page = link.click
@@ -15,20 +15,19 @@ module Adapters
         game_scraper = GameScraper.new
         mechanize = Mechanize.new
         page = mechanize.get("http://gd2.mlb.com/components/game/mlb/")
-        year = click_link(page, "2015")
-        month = click_link(year, "10")
-        binding.pry
+        year = click_link_on(page, "2015")
+        month = click_link_on(year, "10")
         month.links[6..-1].each do |day_page_link|
           day_page = day_page_link.click
-          game_page = click_link(day_page, "gid")
-          inning_index = click_link(game_page, "inning")
-          innings_page = click_link(inning_index, "all")
+          game_page = click_link_on(day_page, "gid")
+          inning_index = click_link_on(game_page, "inning")
+          innings_page = click_link_on(inning_index, "all")
           game_scraper.update_or_create_games(innings_page)
         end
         month.links[6..-1].each do |day_page_link|
           day_page = day_page_link.click
-          game_page = click_link(day_page, "gid")
-          player_index = click_link(game_page, "players")
+          game_page = click_link_on(day_page, "gid")
+          player_index = click_link_on(game_page, "players")
           player_scraper.update_or_create_games(player_index)
         end
      end
