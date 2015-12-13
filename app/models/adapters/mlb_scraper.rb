23a150ad1115c470746546c7ce6e45ff93eb1b16
jrectenwald <jimrec101@gmail.com>
2015-12-13 12:56:41 -0500

    begin rescue block for day navigation

--- app/models/adapters/mlb_scraper.rb
+++ app/models/adapters/mlb_scraper.rb
@@ -2,46 +2,56 @@ module Adapters
   class MlbScraper
 
     def click_link_on(page, info)
-      # page.links.each do |link|
-      #   if link.text.include?(info)
-      #     page = link.click
-      #   end
-      # end
-      # page
-      page.links.collect {|link|link.text.include?(info)}
+      link = page.links.select {|link|link.text.include?(info)}[0]
+      link.click
     end
 
     def page_navigation
-      player_scraper = PlayerScraper.new
-      game_scraper = GameScraper.new
       mechanize = Mechanize.new
       page = mechanize.get("http://gd2.mlb.com/components/game/mlb/")
-      year = click_link_on(page, "2015")
-      month = click_link_on(year, "10")
-      navigate_through_month(game_page) { navigate_through_innings }
-      navigate_through_month(game_page) { navigate_through_players }
+      navigate_through_years(page, 2015, 2015, 10, 10)
     end
 
-    def navigate_through_month(year)
-      month.links[6..-1].each do |day_page_link|
+    def navigate_through_years(page, first_year=2015, final_year=2015, first_month=4, final_month=11)
+      (first_year..final_year).each do |year|
+        year = click_link_on(page, year.to_s)
+        (first_month..final_month).each do |month|
+          month = click_link_on(year, month.to_s)
+          navigate_through_month(month) do |game_page| 
+            unless game_page.links[-1].text == " preview.plist"
+              navigate_through_innings(game_page)
+              navigate_through_players(game_page)
+            end
+          end
+        end
+      end
+    end
+
+    def navigate_through_month(month)
+      month.links[1..-1].each do |day_page_link|
         day_page = day_page_link.click
         day_page.links.each do |day_link|
           if day_link.text.include?("gid")
             game_page = day_link.click
-            yield
+            yield(game_page)
           end
         end
       end
     end
 
     def navigate_through_innings(game_page)
-      inning_index = click_link_on(game_page, "inning")
-      innings_page = click_link_on(inning_index, "all")
+      game_scraper = GameScraper.new
+      begin
+        inning_index = click_link_on(game_page, "inning")
+        innings_page = click_link_on(inning_index, "all")
+      rescue => error
+        binding.pry
+      end
       game_scraper.create_games(innings_page)
     end
 
-    def navigate_through_players(game_page)
-      game_page = day_link.click
+    def navigate_through_players(game_page) 
+      player_scraper = PlayerScraper.new
       player_index = click_link_on(game_page, "players")
       player_scraper.create_players(player_index)
     end
